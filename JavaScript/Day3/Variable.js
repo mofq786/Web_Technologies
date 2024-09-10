7860173a7df691039f192bea5af087f7a69aa214
mofq786 <mofq9786@gmail.com>
2024-09-10 20:45:39 +0530

    JavaScript Day 3. Variables

---
+++ JavaScript/Day3/Variable.js
@@ -0,0 +1,56 @@
+// Dec and Init
+
+// var a = 10;
+// console.log(a);
+// let a = 10;
+// console.log(a);
+// const a = 10;
+// console.log(a);
+
+//Only Dec
+
+// var a;
+// console.log(a);
+// let a;
+// console.log(a);
+// const a;
+// console.log(a);
+
+// Dec and Reinit
+
+// var a;
+// a = 10;
+// console.log(a);
+// let a;
+// a = 10;
+// console.log(a);
+// const a;
+// a = 10;
+// console.log(a);
+
+// Re-Init
+
+// var a = 10;
+// a = "ten";
+// console.log(a);
+// let a = 10;
+// a = "ten";
+// console.log(a);
+// const a = 10;
+// a = "ten";
+// console.log(a);
+
+// Re-Dec
+
+// var a = 10;
+// console.log(a);
+// var a = 20;
+// console.log(a);
+// let a = 10;
+// console.log(a);
+// let a = 20;
+// console.log(a);
+// const a = 10;
+// console.log(a);
+// const a = 20;
+// console.log(a);
