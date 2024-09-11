d7ca6a5dec77f9e2812c88bd12de45894aab6f7c
mofq786 <mofq9786@gmail.com>
2024-09-11 20:41:25 +0530

    Day 3 Primitive Datatype

---
+++ JavaScript/Day4/Variable.js
@@ -0,0 +1,37 @@
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+// console.log(a);
+// const a = 10;
+// console.log(a);
+
+// function num()
+// {
+//     var a = 10;
+// }
+// num();
+// console.log(a);
+
+// Primitive Datatypes
+console.log(typeof 45);
+console.log(typeof 'apple');
+console.log(typeof 'a');
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(null);
+console.log(typeof typeof 45);
+
+var num = 4521357865425452786578;
+console.log(num);
+var num1 = BigInt(1234567890123456789012345);
+console.log(num1,typeof num1);
+
+var foo = Symbol();
+var foo1 = Symbol();
+console.log(foo, typeof foo);
+console.log(foo===foo1);
+
+var num = NaN; // Not a Number
+console.log(typeof num);
